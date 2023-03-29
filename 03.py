bd89c53e4e0720761a2db1c610f08f40b063c4fe
MarkusReithoferStud <126791733+MarkusReithoferStud@users.noreply.github.com>
2023-03-29 12:33:57 +0200

    Update 03.py

--- 03.py
+++ 03.py
@@ -1,47 +1,53 @@
 import sys
 import random
 
-# Aufgabe 06a
 def find_counterexamplesys():
-    tests = 1000000
-    max_val = sys.float_info.max
-    print(max_val)
-    min_val = sys.float_info.min
-    print(min_val)
-    counter = 0
 
-    for i in range(tests):
-        a = random.uniform(min_val, max_val)
-        b = random.uniform(min_val, max_val)
-        c = random.uniform(min_val, max_val)
-#würde code gerne so ändern, dass die for schleife c immer größer macht und 
-#so lange durchgeht bis c sozusagen großgenug ist um ein ergbnis zuerzeugen, dass unterschiedlich groß ist
-# weil so dreimal ranodm ist es nicht systematisch
-        erg1 = (a + b) + c
-        erg2 = a + (b + c)
-        counter += 1
-        print(counter)
+    # Aufgabe 06a
 
-        if erg1 != erg2:
-            print("Gegenbeispiel gefunden:\n")
-            print(f"( {a} + {b} ) + {c} = {erg1}\n")
-            
-            print(f"{a} + ( {b} + {c} ) = {erg2}")
-            return
-        
-    print("Kein Gegenbeispiel gefunden.")
+    # Systematischer Approach. Die Zahlen sind von uns so festgelegt da es anders nicht
+    # funktioniert. Ist dies der richtige Weg? Funktioniert nur mit spezifischen start zahlen.
+    a = 0.1
+    b = 0.0
+    c = 0.0
+
+    while True:
+        z = (a + b) + c
+        y = a + (b + c)
+        if z != y:
+            print(y, z)
+            break
+        a += 0.1
+        if z != y:
+            print(y, z)
+            break
+        b += 0.1
+        if z != y:
+            print(y, z)
+            break
+        c += 0.1
+        if z != y:
+            print(y, z)
+            break
+    print(a, b, c)
+    print("----------------------------")
+    # Aufgabe 06b
+    # Bei diesen Zahlen ist das Ergebnis nicht gleich.
+    # Dies würde Punkt zwei beantworten.
+    a = 0.4
+    b = 0.1
+    c = 0.2
+    z = (a + b) + c
+    y = a + (b + c)
+    print(z, y)
+    if z == y:
+        print("Das Ergebnis ist gleich")
+    else:
+        print("Das Ergebnis ist nicht gleich")
 
 find_counterexamplesys()
 
-#print(pi)
-#print(e)
-
-erg1 = (1e-308 + 1e308) + 1
-erg2 = 1e-308 + (1e308 + 1)
-print(1e-35 + 1e35)
-print(erg1)
-print(erg2)
-
-## versteh nicht warum er mir da immer noch float true ausgibt
-
-print(erg1 == erg2)
+def mean_naive (X):
+    sum = 0
+    for i in X:
+        pass
